1b595f4a7ee5caccaab30de5398bdc2491363486
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-03-02 16:44:25 +0200

    Indicator filtering for breakdown by SO chart

--- scoreboard/visualization/jsapp/samples/breakdown_by_so.js
+++ scoreboard/visualization/jsapp/samples/breakdown_by_so.js
@@ -24,20 +24,22 @@
     {
       "constraints": {},
       "default_value": "#random",
+      "ignore_values": ["mobile", "internet-usage", "audiovisual", "egovernment", "ecommerce", "ebusiness", "ict-skills", "ict-sector", "research_eInfra"],
       "dimension": "indicator-group",
-      "include_wildcard": true,
+      "include_wildcard": false,
       "label": "Indicator group",
       "name": "indicator-group",
       "position": "upper-left",
       "sortBy": "order_in_codelist",
       "sortOrder": "asc",
-      "type": "select"
+      "type": "hidden_select"
     },
     {
       "constraints": {
         "indicator-group": "indicator-group"
       },
       "default_value": "#random",
+      "ignore_values": ["spectrum_WBB", "pay_on_time_projects", "avg_ttg", "ttg_in_scope", "epsi_score", "e_cc_pcpu", "e_CC_GE_ME", "e_cc", "eoa_VoD_estab_otheu", "eoa_Tvchannels_tarfor", "ICT_commit_in_trp", "i_iuse", "i_iu3", "i_ilt12", "i_bfeu", "i_iugov12", "eoa_nn_films", "e_rfpsas", "E_ITSPVAC2", "ict_hgc", "c1_compcyclesprace", "c1_compcyclesegi", "c1_datatraffic", "c1_bandwidth"],
       "dimension": "indicator",
       "label": "Indicator",
       "name": "indicator",
@@ -121,11 +123,7 @@
         "ref-area": "ref-area"
       },
       "dimension": "breakdown",
-      "default_value": [
-        "#random",
-        "#random",
-        "#random"
-      ],
+      "default_value": ["SO_01","SO_02","SO_03","SO_04","SO_05","SO_06","SO_07","SO_08","SO_09","SO_10","SO_11","SO_12","SO_13","SO_14","SO_15","SO_16","SO_17","SO_18","SO_19","SO_20","SO_21","SO_22_1","SO_22_2","SO_22_3","SO_23","SO_24","SO_25","SO_26","eInfra","Total_SOs"],
       "highlights": ["Total_SOs"],
       "label": "Breakdown",
       "name": "breakdown",
