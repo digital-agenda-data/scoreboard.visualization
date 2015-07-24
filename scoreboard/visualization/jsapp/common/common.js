a0754292b1b74964075200b23655d4a7dc12f340
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-07-24 23:41:09 +0300

    Added total to list of notation_totals

--- scoreboard/visualization/jsapp/common/common.js
+++ scoreboard/visualization/jsapp/common/common.js
@@ -147,6 +147,6 @@ App.isIE78 = function () {
     return false;
 }
 
-App.notation_totals = ["1M_websites", "HH_total", "IND_TOTAL", "RURAL_POP", "TOTAL", "TOTAL_FBB", "TOTAL_MBB", "TOTAL_MOB", "TOTAL_POP","TOTAL_POPHH", "TOTAL_TEL", "ent_all_xfin", "orgclass_total", "tot_ict_products", "total_offers8plus", "all_eGov_le"];
+App.notation_totals = ["1M_websites", "HH_total", "IND_TOTAL", "RURAL_POP", "TOTAL", "TOTAL_FBB", "TOTAL_MBB", "TOTAL_MOB", "TOTAL_POP","TOTAL_POPHH", "TOTAL_TEL", "ent_all_xfin", "orgclass_total", "tot_ict_products", "total_offers8plus", "all_eGov_le", "total"];
 
 })(App.jQuery);
